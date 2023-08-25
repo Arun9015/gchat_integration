dd592ad64f3d32d97e7437f55b3621a5438200c7
arun <arun.kumar@impressico.com>
2023-08-25 13:10:31 +0530

    testing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ repo = g.get_repo(os.environ['REPO_NAME'])
 # GitHub token with appropriate permissions
 # GITHUB_TOKEN = "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"
 
-# Google Chat webhook URL
+# Google Chat webhook URL.
 GCHAT_WEBHOOK_URL = "https://chat.googleapis.com/v1/spaces/AAAAJWnA1jA/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=pfdOLCmNOvrazVV-IkSORjSpmfOs9Kpay03eCZD5xww"
 
 def main():
